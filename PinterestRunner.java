b6f4504facb6955f17119e4404d953ec05c457f1
git practice files <udaykiran162003@gmail.com>
2025-03-19 13:38:43 +0530

    I am adding a file related to methods concept.

---
+++ PinterestRunner.java
@@ -0,0 +1,9 @@
+class PinterestRunner{
+	
+	public static void main(String a[]){
+	
+	String msg = Pinterest.signUp("Charles@gmail.com" , "12345678" , "21/2/2000");
+	System.out.println(msg);
+	}
+
+}
