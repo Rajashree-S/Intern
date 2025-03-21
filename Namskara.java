050b1b20831ccf9de5bcf8c279f47d5f37b8f35b
Rajashree-S <rajashree.madhu27@gmail.com>
2025-03-21 10:57:03 +0530

    First program.

---
+++ Namskara.java
@@ -0,0 +1,9 @@
+class Namskara{
+	
+	public static void main(String args[]){
+		
+		System.out.println("Bani java kalyano...");
+
+	}
+
+}
--- PinterestRunner.java
+++
@@ -1,9 +0,0 @@
-class PinterestRunner{
-	
-	public static void main(String a[]){
-	
-	String msg = Pinterest.signUp("Charles@gmail.com" , "12345678" , "21/2/2000");
-	System.out.println(msg);
-	}
-
-}
